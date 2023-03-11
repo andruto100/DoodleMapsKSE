3408614a63ee5fd86f0284c618d72276504ccb64
Andruto100 <adatsiv@kse.org.ua>
2023-03-11 04:58:39 +0200

    0.1

--- DoodleMaps/Program.cs
+++ DoodleMaps/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Kse.Algorithms.Samples;
 
-Console.WriteLine("Hello, World!");
+var generator = new MapGenerator(new MapGeneratorOptions()
+{
+    Height = 35,
+    Width = 90,
+});
+
+string[,] map = generator.Generate();
+new MapPrinter().Print(map);
+List<Point> GetShortestPath(string[,] map, Point start, Point goal)
+{
+   
+}
